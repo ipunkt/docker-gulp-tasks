49c41fbb216623b37907e64204d9578c528842ef
piJoe <piJoe@users.noreply.github.com>
2016-05-03 14:07:46 +0200

    * added npm scripts for build, build:dev and watch * added needed dependencies to package.json, removed some old

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,4 +1,13 @@
+var gulp = require('gulp');
 var requireDir = require('require-dir');
+var gulpSequence = require('gulp-sequence');
 
 requireDir('./tasks', {recurse: true});
 require('./project/gulpfile.js');
+
+var buildDev = function(cb) {
+    global.development = true;
+    gulpSequence('build', cb);
+};
+
+gulp.task('build:dev', buildDev);
