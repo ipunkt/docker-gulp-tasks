a114c166c96c05be81ac8879d525431c1a14bb4f
piJoe <piJoe@users.noreply.github.com>
2016-05-03 15:37:35 +0200

    Fix config path

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var requireDir = require('require-dir');
 var gulpSequence = require('gulp-sequence');
-global.config = require('./gulp_config.json');
+global.config = require('./project/gulp_config.js');
 
 requireDir('./tasks', {recurse: true});
 require('./project/gulpfile.js');
