72da8c8cc5ae9ed4de1430ff1981504c52516cef
piJoe <piJoe@users.noreply.github.com>
2016-05-03 15:28:40 +0200

    - Added css task     - Compiles sass, scss to css     - sourcemaps on build:dev     - autoprefixer     - cssnano for minifying     - browserSync on build:dev
    - Extended the gulp_config.json with examples for the css task
    - Added css task to build in example gulp file
    
    - Load the config in our internal gulpfile globally.

---
+++ gulp/tasks/css.js
@@ -0,0 +1,28 @@
+'use strict';
+
+if(!config.tasks.css) return;
+
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+var path = require('path');
+
+var paths = {
+    src: path.join(config.root.src, config.tasks.css.src, '/**/*.{' + config.tasks.css.extensions + '}'),
+    dest: path.join(config.root.dest, config.tasks.css.dest)
+};
+
+gulp.task('css', function() {
+    return gulp.src(paths.src)
+        .pipe(gulpif(global.development, sourcemaps.init()))
+        .pipe(sass(config.tasks.css.sass).on('error', sass.logError))
+        .pipe(autoprefixer(config.tasks.css.autoprefixer))
+        .pipe(gulpif(!global.development, cssnano({autoprefixer: false})))
+        .pipe(gulpif(global.development, sourcemaps.write()))
+        .pipe(gulp.dest(paths.dest))
+        .pipe(gulpif(global.development, browserSync.stream()))
+});
