1c4ec9cfc2b2873ee4189deefa936de2a67776c5
piJoe <piJoe@users.noreply.github.com>
2016-06-03 15:02:53 +0200

    add webpack:watch
    add webpack:watch task that utilises the webpack watch function on poll mode
    with a 500ms interval.

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -6,18 +6,40 @@ var gulp = require("gulp");
 var gutil = require("gulp-util");
 var webpack = require("webpack");
 
-gulp.task("webpack", function(callback) {
+function webpackTask(watch, callback) {
     //chdir to our project path to make webpack work as intended
     var oldPath = process.cwd();
     process.chdir(projectPath);
 
-    //run webpack
-    webpack(config.webpack, function(err, stats) {
+    //setup webpack
+    var w = webpack(config.webpack);
+
+    function webpackCallback(err, stats) {
         if (err) throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString());
         callback();
-    });
+    }
+
+    //run webpack either in watch mode or normal (once)
+    if (watch) {
+        w.watch({
+            aggregateTimeout: 500,
+            poll: 500
+        }, webpackCallback);
+    } else {
+        w.run(webpackCallback);
+    }
 
     //go back to our old path
     process.chdir(oldPath);
+}
+
+
+gulp.task("webpack", function(callback) {
+    return webpackTask(false, callback);
+});
+
+
+gulp.task("webpack:watch", function(callback) {
+    return webpackTask(true, callback);
 });
